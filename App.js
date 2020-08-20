467faa40a1eb8f861eef3ceb6dd542f7f3002e1a
egiacalone <egiacalone@deloitte.it>
2020-08-20 12:02:46 +0200

    [ env-manager ] - added env manager

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import AppContainer from './src/navigations/AppNavigation';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AppContainer from "./src/navigations/AppNavigation";
+import Config from "react-native-config";
 
 export default function App() {
-  return (
-     <AppContainer />
-  );
+  const env = Config.ENV;
+  return <AppContainer />;
 }

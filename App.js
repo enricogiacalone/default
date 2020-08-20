9b7ccf404dcf9fd1c425f8d83492dfd621a283d8
egiacalone <egiacalone@deloitte.it>
2020-08-20 14:29:05 +0200

    [ splashScreen ] - added splashScreen

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import AppContainer from "./src/navigations/AppNavigation";
 import Config from "react-native-config";
+import SplashScreen from "react-native-splash-screen";
 
-export default function App() {
-  const env = Config.ENV;
-  return <AppContainer />;
+export default class App extends Component {
+
+  componentDidMount() {
+    SplashScreen.hide()
+  }
+  
+  render() {
+    const env = Config.ENV;
+    return <AppContainer />;
+  }
 }
--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,33 +1,44 @@
-import React from 'react';
-import { FlatList, ScrollView, Text, View, TouchableHighlight, Image } from 'react-native';
-import styles from './styles';
-import { recipes } from '../../data/dataArrays';
-import MenuImage from '../../components/MenuImage/MenuImage';
-import DrawerActions from 'react-navigation';
-import { getCategoryName } from '../../data/MockDataAPI';
+import React from "react";
+import {
+  FlatList,
+  ScrollView,
+  Text,
+  View,
+  TouchableHighlight,
+  Image,
+  StatusBar,
+} from "react-native";
+import styles from "./styles";
+import { recipes } from "../../data/dataArrays";
+import MenuImage from "../../components/MenuImage/MenuImage";
+import DrawerActions from "react-navigation";
+import { getCategoryName } from "../../data/MockDataAPI";
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = ({ navigation }) => ({
-    title: 'Home',
+    title: "Home",
     headerLeft: (
       <MenuImage
         onPress={() => {
           navigation.openDrawer();
         }}
       />
-    )
+    ),
   });
 
   constructor(props) {
     super(props);
   }
 
-  onPressRecipe = item => {
-    this.props.navigation.navigate('Recipe', { item });
+  onPressRecipe = (item) => {
+    this.props.navigation.navigate("Recipe", { item });
   };
 
   renderRecipes = ({ item }) => (
-    <TouchableHighlight underlayColor='rgba(73,182,77,1,0.9)' onPress={() => this.onPressRecipe(item)}>
+    <TouchableHighlight
+      underlayColor="rgba(73,182,77,1,0.9)"
+      onPress={() => this.onPressRecipe(item)}
+    >
       <View style={styles.container}>
         <Image style={styles.photo} source={{ uri: item.photo_url }} />
         <Text style={styles.title}>{item.title}</Text>
@@ -39,13 +50,14 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View>
+        <StatusBar barStyle="dark-content" backgroundColor="#4F6D7A" />
         <FlatList
           vertical
           showsVerticalScrollIndicator={false}
           numColumns={2}
           data={recipes}
           renderItem={this.renderRecipes}
-          keyExtractor={item => `${item.recipeId}`}
+          keyExtractor={(item) => `${item.recipeId}`}
         />
       </View>
     );
